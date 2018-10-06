3c2c7c5d75ba868e6f907801c00282e6b09cc221
= <=>
2018-10-05 22:15:29 -0300

    Game files

---
+++ Assets/Scripts/GameMechanic.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameMechanic : MonoBehaviour {
+
+	[SerializeField] int max;  
+	[SerializeField] int min; 
+	[SerializeField] int numberGuessesAllowed;
+	[SerializeField] Text guessedNumber;
+	[SerializeField] Text numberOfGuesses;
+	[SerializeField] string loseScreen;
+
+	int guessMade;
+
+	public SceneController sceneController; //to get the method of the script SceneController
+
+	// Starts the game with the first guess
+	void Start () {
+		NextGuess();
+	}
+
+	//Method for when the number guessed should be higher
+	public void HigherNumber(){
+		min = guessMade + 1;
+		NextGuess();
+	}
+
+	//Method for when the number guessed should be lower
+	public void LowerNumber(){
+		max = guessMade - 1;
+		NextGuess();
+	}
+	
+	// Method for the guess and print the result and number of guesses left on screen
+	public void NextGuess () {
+		guessMade = Random.Range(min, max+1);  
+		guessedNumber.text = guessMade.ToString();
+		numberOfGuesses.text = numberGuessesAllowed.ToString();
+		if(numberGuessesAllowed <= 0){ //loses game when there are no more tries left
+			sceneController.LoadTheScene(loseScreen);
+		}
+		numberGuessesAllowed--;
+	}
+
+	//Method to load the correct winning scene for the correct button
+	public void CorrectGuess(){
+		sceneController.ChooshingWinScene(numberGuessesAllowed);
+	}
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+	string firstWinScene = "WinFirstScene";
+	string	secondWinScene="WinSecondScene";
+	string	thirdWinScene="WinThirdScene";
+	
+	
+	//Method to load scene "Name" on click of button
+	public void LoadTheScene(string name){
+		SceneManager.LoadScene(name, LoadSceneMode.Single);
+	}
+	
+	//Method to quit the game when button of quit is clicked
+	public void QuitTheGame(){
+		Application.Quit();
+	}
+
+
+	//Method to choose a winning scene when the button is clicked based on the number of tries to guess
+	public void ChooshingWinScene(int numberGuessesAllowed_reference){ //number of tries left - 1
+		if(numberGuessesAllowed_reference==0){ //won on the last try
+			LoadTheScene(firstWinScene);
+		}
+		else if(numberGuessesAllowed_reference>1 && numberGuessesAllowed_reference<5){ //won with left tries between 2 and 5
+			LoadTheScene(secondWinScene);			
+		}
+		else{ 
+			LoadTheScene(thirdWinScene);			
+		}
+	}	
+
+}
